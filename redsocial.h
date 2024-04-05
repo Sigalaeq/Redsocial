71f83d5d69e4b1e3d467ee7ad2ab30b6d90d2b73
Sigalaeq <13039portalcedi.edu.mx>
2024-04-05 13:11:07 -0600

    segundo

--- redsocial.h
+++ redsocial.h
@@ -1,13 +1,28 @@
 #ifndef redsocial_h
- #definde redsocial_h
- #include "usuario.h"
+ #define  redsocial_h    
+ #include "Usuario.h"
  #include "publicacion.h" 
 #include <cstdlib> 
+#include <vector>
+#include <iostream>
 using namespace std;
 
 class Redsocial {
     private: 
+    vector<Usuario*>usuarios;
+    vector<Publicacion>publicaciones;
+    public:
+    string nombre;
+    int numusers;
+    int numpubli;
+    void agregarUsuario();
+    void mostrarUsuario();
+    void mostrarPublicaciones();
+    Usuario* getUsuario(int  id);
+    Redsocial(string nombre);
+    Redsocial(string nombre,vector<Usuario*>usuarios);
+    Redsocial(string nombre,vector<Usuario*>usuarios,vector<Publicacion>publicaciones);
+    
 
 
-}
-
+#endif
