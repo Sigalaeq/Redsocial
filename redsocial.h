07938072538c630b70ac79e5e44952c7df957d01
Sigalaeq <13039portalcedi.edu.mx>
2024-04-05 12:10:57 -0600

    inicio

---
+++ redsocial.h
@@ -0,0 +1,13 @@
+#ifndef redsocial_h
+ #definde redsocial_h
+ #include "usuario.h"
+ #include "publicacion.h" 
+#include <cstdlib> 
+using namespace std;
+
+class Redsocial {
+    private: 
+
+
+}
+
